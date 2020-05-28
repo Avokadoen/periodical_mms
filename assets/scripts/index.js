a9aa8b592061c3d10d74224375ecb316b18fa083
Aksel <akselhallajs@gmail.com>
2020-05-28 12:49:31 +0200

    build for prod, test fetch method

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,39 @@
-//import { fromEvent } from "rxjs";
+import { fromEvent, from } from "rxjs";
+import { exhaustMap, delay } from "rxjs/operators"
 
-//fromEvent(document, 'DOMContentLoaded').subscribe(() => console.log('coolio'));
-
-document.addEventListener("DOMContentLoaded", main);
+fromEvent(document, 'DOMContentLoaded').subscribe(main);
 
 function main() {
-    // 
-    const searchButton =  document.getElementById("objectSearchBtn");
-    searchButton.addEventListener("click", () => console.log('jippi'));
+    let state = {
+        // All elements relevant to the object id form
+        search_elements: {
+            input: document.getElementById("objectIdForm"),
+            button: document.getElementById("objectSearchBtn"),
+            spinner: document.getElementById("objectSearchSpinner"),
+            helper: document.getElementById("objectIdFormHelp"),
+        }
+    }
+
+    fromEvent(state.search_elements.button, "click").pipe(
+        delay(400),
+        exhaustMap(ev => fetchEntry("71518658360002201"))        
+    ).subscribe(console.log);
+
+}
+
+function fetchEntry(id) {
+    const sru_url = "https://bibsys.alma.exlibrisgroup.com/view/sru/47BIBSYS_NETWORK"
+                  + "?version=1.2&operation=searchRetrieve&recordSchema=marcxml&maximumRecords=1"
+                  + "&query=alma.granular_resource_type=p AND alma.all_for_ui="
+    const fetch_url = sru_url + id;
+
+    return from(
+        fetch(fetch_url, { 
+            method: 'GET',  
+            mode: 'cors',
+            Origin: 'https://avokadoen.github.io/periodical_mms',
+            Accept: 'application/xhtml+xml,application/xml',
+        }).then(response => response)
+    );
+    
 }
